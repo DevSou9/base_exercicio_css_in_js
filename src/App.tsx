e0a4162d8a9ac0afe79c2c64792103153a453bca
W Souza <dev9.ws@gmail.com>
2024-02-17 21:03:39 -0300

    Styled-components Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
+import { ThemeProvider } from 'styled-components'
+import { Tema } from './theme/theme'
 import './global.css'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={Tema}>
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
